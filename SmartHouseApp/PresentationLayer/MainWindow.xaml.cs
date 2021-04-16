ed6ffe412da300f5a0b95a8e471419050682112d
Ziemniak <potatoosalad@gmail.com>
2021-04-16 21:25:24 +0200

    ViewModel Example class

--- SmartHouseApp/PresentationLayer/MainWindow.xaml.cs
+++ SmartHouseApp/PresentationLayer/MainWindow.xaml.cs
@@ -20,14 +20,11 @@ namespace PresentationLayer
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
+            this.DataContext = VMExample;
         }
     }
 }
---
+++ SmartHouseApp/PresentationLayer/ViewModelExample.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PresentationLayer
+{
+    public partial class MainWindow : Window
+    {
+        private ViewModelExample VMExample { get; set; } = new ViewModelExample() { Name = "Jan Paweł II Papież", Age = 2137 };
+    }
+
+    class ViewModelExample : INotifyPropertyChanged
+    {
+        private string name;
+        private int age;
+
+        private ExampleCommand command;
+
+
+        public ExampleCommand Command
+        {
+            get
+            {
+                if(command == null)
+                {
+                    command = new ExampleCommand(this);
+                }
+                return command;
+            }
+            set
+            {
+                command = value;
+            }
+        }
+
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Age"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    class ExampleCommand : ICommand
+    {
+        private ViewModelExample vMField;
+        public event EventHandler CanExecuteChanged;
+
+        public ExampleCommand(ViewModelExample VMField) => this.vMField = VMField;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            vMField.Name = DateTime.Now.ToString();
+        }
+    }
+}
