b9ec021ce503cc8e427bd0fe63bde2680c083df3
Ziemniak <potatoosalad@gmail.com>
2021-04-17 15:42:34 +0200

    Navigation between pages, Schedule page view concept

---
+++ SmartHouseApp/PresentationLayer/ViewModels/NavigationViewModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PresentationLayer.ViewModels
+{
+    class NavigationViewModel : BaseViewModel
+    {
+        public NavigationViewModel()
+        {
+            Devices = new DevicesCommand(this);
+            Schedule = new ScheduleCommand(this);
+            Alerts = new AlertsCommand(this);
+            LoadDevices();
+        }
+
+        #region ViewModel
+        private string currentPage;
+        public string CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                currentPage = value;
+                OnPropertyChanged("CurrentPage");
+            }
+        }
+        #endregion
+
+        #region ICommands
+        public ICommand Devices { get; private set; }
+        public void LoadDevices()
+        {
+            CurrentPage = "DevicesPage.xaml";
+        }
+
+        public ICommand Schedule { get; private set; }
+        public void LoadSchedule()
+        {
+            CurrentPage = "";
+        }
+
+        public ICommand Alerts { get; private set; }
+        public void LoadAlerts()
+        {
+            CurrentPage = "";
+        }
+        #endregion
+    }
+
+    class DevicesCommand : ICommand
+    {
+        private readonly NavigationViewModel navigationViewModel;
+
+        public DevicesCommand(NavigationViewModel navigationViewModel)
+        {
+            this.navigationViewModel = navigationViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            navigationViewModel.LoadDevices();
+        }
+    }
+
+    class ScheduleCommand : ICommand
+    {
+        private readonly NavigationViewModel navigationViewModel;
+
+        public ScheduleCommand(NavigationViewModel navigationViewModel)
+        {
+            this.navigationViewModel = navigationViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            navigationViewModel.LoadSchedule();
+        }
+    }
+
+    class AlertsCommand : ICommand
+    {
+        private readonly NavigationViewModel navigationViewModel;
+
+        public AlertsCommand(NavigationViewModel navigationViewModel)
+        {
+            this.navigationViewModel = navigationViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            navigationViewModel.LoadAlerts();
+        }
+    }
+}
--- SmartHouseApp/PresentationLayer/Views/SchedulePage.xaml.cs
+++ SmartHouseApp/PresentationLayer/Views/SchedulePage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,15 +14,13 @@ using System.Windows.Shapes;
 namespace PresentationLayer.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SchedulePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SchedulePage : Page
     {
-        ViewModels.NavigationViewModel navigationViewModel { get; set; } = new ViewModels.NavigationViewModel();
-        public MainWindow()
+        public SchedulePage()
         {
             InitializeComponent();
-            this.DataContext = navigationViewModel;
         }
     }
 }
