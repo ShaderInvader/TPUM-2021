3479625007678271483ca159de7495f61639a652
Ziemniak <potatoosalad@gmail.com>
2021-04-17 20:40:27 +0200

    UI Adding devices WIP

--- SmartHouseApp/PresentationLayer/ViewModels/DeviceViewModel.cs
+++ SmartHouseApp/PresentationLayer/ViewModels/DeviceViewModel.cs
@@ -1,21 +1,28 @@
 ﻿using LogicLayer.DTOs;
+using LogicLayer.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace PresentationLayer.ViewModels
 {
     class DeviceViewModel : BaseViewModel
     {
         private ObservableCollection<DeviceDTO> devices;
-
         private DeviceDTO selectedDevice;
+        //private readonly DeviceService deviceService;
+        private bool newDevice;
 
         public DeviceViewModel()
         {
             devices = new ObservableCollection<DeviceDTO>(MOCKs.DevicesMock.devicesMock);
+            selectedDevice = devices[0];
+            newDevice = false;
+            NewDeviceCommand = new NewDeviceCommand(this);
+            SaveDeviceCommand = new SaveDeviceCommand(this);
         }
 
         public ObservableCollection<DeviceDTO> Devices
@@ -37,6 +44,75 @@ namespace PresentationLayer.ViewModels
                 OnPropertyChanged("SelectedDevice");
             }
         }
+
+        //public DeviceService DeviceService
+        //{
+        //    get => deviceService;
+        //}
+
+        public bool NewDevice
+        {
+            get => newDevice;
+            set
+            {
+                newDevice = value;
+                OnPropertyChanged("NewDevice");
+                OnPropertyChanged("OldDevice");
+            }
+        }
+
+        public bool OldDevice
+        {
+            get => !NewDevice;
+        }
+
+        #region ICommands
+        public ICommand NewDeviceCommand { get; set; }
+        public ICommand SaveDeviceCommand { get; set; }
+        #endregion
     }
 
+    class NewDeviceCommand : ICommand
+    {
+        private readonly DeviceViewModel deviceViewModel;
+        public NewDeviceCommand(DeviceViewModel deviceViewModel)
+        {
+            this.deviceViewModel = deviceViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return deviceViewModel.Devices.Count > 0;
+        }
+
+        public void Execute(object parameter)
+        {
+            deviceViewModel.NewDevice = true;
+            deviceViewModel.SelectedDevice = new DeviceDTO();
+        }
+    }
+
+    class SaveDeviceCommand : ICommand
+    {
+        private readonly DeviceViewModel deviceViewModel;
+        public SaveDeviceCommand(DeviceViewModel deviceViewModel)
+        {
+            this.deviceViewModel = deviceViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return deviceViewModel.Devices.Count > 0;
+        }
+
+        public void Execute(object parameter)
+        {
+            deviceViewModel.NewDevice = false;
+            deviceViewModel.SelectedDevice = deviceViewModel.Devices[deviceViewModel.Devices.Count - 1];
+        }
+    }
 }
