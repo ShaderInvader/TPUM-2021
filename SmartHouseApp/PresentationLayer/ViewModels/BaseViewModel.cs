775a9a116cf872257f41ee6684dc38e38fcc24c3
Ziemniak <potatoosalad@gmail.com>
2021-04-17 15:02:01 +0200

    Devices Page concept, BaseViewModel, Presentation Layer classes moved to directory tree

---
+++ SmartHouseApp/PresentationLayer/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace PresentationLayer.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
