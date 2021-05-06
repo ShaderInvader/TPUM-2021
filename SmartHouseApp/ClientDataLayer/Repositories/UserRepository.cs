89c06741f4252ad9b1c29bbc4971b436abe0b12a
Filip Mazurek <f.maz.98@outlook.com>
2021-05-06 16:18:27 +0200

    Added WebSocketClient and WebSocketConnection

---
+++ SmartHouseApp/ClientDataLayer/Repositories/UserRepository.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ModelCommon;
+using ModelCommon.Interfaces;
+
+namespace ClientDataLayer
+{
+    public class UserRepository : INamedRepository<User>
+    {
+        private readonly DataContext _dataContext;
+
+        public UserRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public IEnumerable<User> Get()
+        {
+            return _dataContext.Users;
+        }
+
+        public User Get(int id)
+        {
+            return _dataContext.Users.Find(user => user.Id == id);
+        }
+
+        public void Add(User item)
+        {
+            _dataContext.Users.Add(item);
+        }
+
+        public int Remove(int id)
+        {
+            return _dataContext.Users.RemoveAll(user => user.Id == id);
+        }
+
+        public bool Remove(User item)
+        {
+            return _dataContext.Users.Remove(item);
+        }
+
+        public bool Update(int id, User item)
+        {
+            User found = _dataContext.Users.Find(user => user.Id == id);
+            if (found != null)
+            {
+                found.Email = item.Email;
+                found.Name = item.Name;
+                found.FirstName = item.FirstName;
+                found.LastName = item.LastName;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public User Get(string name)
+        {
+            return _dataContext.Users.Find(user => user.Name == name);
+        }
+
+        public IEnumerable<User> GetAll(string name)
+        {
+            return _dataContext.Users.FindAll(user => user.Name == name);
+        }
+
+        public int GetFirstId(string name)
+        {
+            User found = _dataContext.Users.Find(user => user.Name == name);
+            if (found != null)
+            {
+                return found.Id;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int[] GetIds(string name)
+        {
+            List<User> found = _dataContext.Users.FindAll(user => user.Name == name);
+            int[] ids = new int[found.Count];
+            for (int i = 0; i < found.Count; i++)
+            {
+                ids[i] = found[i].Id;
+            }
+
+            return ids;
+        }
+
+        public int Remove(string name)
+        {
+            return _dataContext.Users.RemoveAll(user => user.Name == name);
+        }
+
+        public bool UpdateFirst(string name, User item)
+        {
+            User found = _dataContext.Users.Find(user => user.Name == name);
+            if (found != null)
+            {
+                found.Email = item.Email;
+                found.Name = item.Name;
+                found.FirstName = item.FirstName;
+                found.LastName = item.LastName;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int UpdateAll(string name, User item)
+        {
+            List<User> found = _dataContext.Users.FindAll(user => user.Name == name);
+            foreach (var t in found)
+            {
+                t.Email = item.Email;
+                t.Name = item.Name;
+                t.FirstName = item.FirstName;
+                t.LastName = item.LastName;
+            }
+            return found.Count;
+        }
+    }
+}
---
+++ SmartHouseApp/ClientDataLayer/WebSocketClient.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientDataLayer
+{
+    public static class WebSocketClient
+    {
+        public static async Task<WebSocketConnection> Connect(Uri peer, Action<string> log)
+        {
+            ClientWebSocket clientWebSocket = new ClientWebSocket();
+
+            await clientWebSocket.ConnectAsync(peer, CancellationToken.None);
+
+            switch (clientWebSocket.State)
+            {
+                case WebSocketState.Open:
+                    log?.Invoke($"Opened connection to remote server: {peer}");
+                    WebSocketConnection socketConnection = new ClientWebSocketConnection(clientWebSocket, peer, log);
+                    return socketConnection;
+                    
+                default:
+                    string errMsg = $"Cannot connect to remote server {peer} with status {clientWebSocket.State}";
+                    log?.Invoke(errMsg);
+                    throw new WebSocketException(errMsg);
+            }
+        }
+
+        public class ClientWebSocketConnection : WebSocketConnection
+        {
+            private ClientWebSocket _clientWebSocket = null;
+            private Uri _peer = null;
+            private readonly Action<string> log;
+
+            public ClientWebSocketConnection(ClientWebSocket clientWebSocket, Uri peer, Action<string> log)
+            {
+                this.log = log;
+                _peer = peer;
+                _clientWebSocket = clientWebSocket;
+                Task.Factory.StartNew(() => ClientMessageLoop());
+            }
+
+            public override Task DisconnectAsync()
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override Task SendTask(string message)
+            {
+                throw new NotImplementedException();
+            }
+
+            private void ClientMessageLoop()
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    while (true)
+                    {
+                        ArraySegment<byte> segment = new ArraySegment<byte>(buffer);
+                        WebSocketReceiveResult result = _clientWebSocket.ReceiveAsync(segment, CancellationToken.None).Result;
+                        if (result.MessageType == WebSocketMessageType.Close)
+                        {
+                            onClose?.Invoke();
+                            _clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "I am closing", CancellationToken.None).Wait();
+                            return;
+                        }
+                        int count = result.Count;
+                        while (!result.EndOfMessage)
+                        {
+                            if (count >= buffer.Length)
+                            {
+                                onClose?.Invoke();
+                                _clientWebSocket.CloseAsync(WebSocketCloseStatus.InvalidPayloadData, "That's too long", CancellationToken.None).Wait();
+                                return;
+                            }
+                            segment = new ArraySegment<byte>(buffer, count, buffer.Length - count);
+                            result = _clientWebSocket.ReceiveAsync(segment, CancellationToken.None).Result;
+                            count += result.Count;
+                        }
+                        string _message = Encoding.UTF8.GetString(buffer, 0, count);
+                        onMessage?.Invoke(_message);
+                    }
+                }
+                catch (Exception _ex)
+                {
+                    log($"Connection has been broken because of an exception {_ex}");
+                    _clientWebSocket.CloseAsync(WebSocketCloseStatus.InternalServerError, "Connection has been broken because of an exception", CancellationToken.None).Wait();
+                }
+            }
+        }
+    }
+}
